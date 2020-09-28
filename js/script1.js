b2217f914fa0729d25498bdbd42c26bec9dcab6c
Pluwka <htc.loopback@gmail.com>
2020-09-28 19:26:56 +0300

    four all

---
+++ js/script1.js
@@ -0,0 +1,22 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for (let i = 0; i <1; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length <50) {
+        
+    }
+    
+    personalMovieDB.movies[a] = b;
+}
+
+console.log(personalMovieDB);
