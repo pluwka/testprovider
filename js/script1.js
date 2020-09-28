fd8bf08982c3aa32c5f85a21461a6b64f05243db
Pluwka <htc.loopback@gmail.com>
2020-09-28 20:18:45 +0300

    new

--- js/script1.js
+++ js/script1.js
@@ -1,4 +1,4 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '20');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -9,14 +9,26 @@ const personalMovieDB = {
 };
 
 for (let i = 0; i <1; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
+    const a = prompt('Один из последних просмотренных фильмов?', 'Nazi'),
+          b = prompt('На сколько оцените его?', '14,88');
 
     if (a != null && b != null && a != '' && b != '' && a.length <50) {
-        
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
     }
-    
-    personalMovieDB.movies[a] = b;
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
 }
 
 console.log(personalMovieDB);
